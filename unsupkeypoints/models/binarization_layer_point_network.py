0f4526349350d87b0d5f87e3b626ccd16594389e
Mikhail Kurenkov <Mikhail.Kurenkov@skoltech.ru>
2021-07-26 14:28:42 +0300

    Add probabilistic parameter

--- unsupkeypoints/models/binarization_layer_point_network.py
+++ unsupkeypoints/models/binarization_layer_point_network.py
@@ -1,20 +1,27 @@
 from .base_lightning_module import BaseLightningModule
 import torch
+import torch.autograd
 
 
 class BinarizationPointNetwork(BaseLightningModule):
-    def __init__(self, parameters, classifier, regressor, criterion):
+    def __init__(self, parameters, classifier, regressor, criterion, probabilistic=True):
         super().__init__(parameters)
         self._classifier = classifier
         self._regressor = regressor
         self._criterion = criterion
+        self._probabilistic = probabilistic
 
     def forward(self, x):
         x = self._classifier(x)
         probability = torch.sigmoid(x)
-        a = torch.bernoulli(probability)
-        c = (a - probability).detach()
-        x = c + probability
+        if self._probabilistic and self.training:
+            a = torch.bernoulli(probability).detach()
+            c = (a - probability).detach()
+            x = c + probability
+        elif self._probabilistic:
+            x = torch.where(probability > 0.5, 1., 0.)
+        else:
+            x = probability
         x = self._regressor(x)
         return x
 
